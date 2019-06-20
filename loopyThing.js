69e158551f0d0df0cb3eb04b0876d38b8b795752
Mariacruz4 <eloisa943@gmail.com>
2019-06-20 00:20:30 -0500

    Added function

--- loopyThing.js
+++ loopyThing.js
@@ -1,6 +1,12 @@
-if(loopThing > 10){
-    console.log("Hey! That's a large number! ");
-}
-else{
-    console.log("Hey! That's a small number!");
-}
+function loopyThing(num){
+    if(num > 10){
+       console.log("Hey! That's a large number! ");
+   }
+   else{
+       console.log("Hey! That's a small number!");
+   }   
+   }
+   
+    loopyThing(5)
+    console.log("===========================")
+    loopyThing(12)
